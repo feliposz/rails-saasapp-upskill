f9474141e4a76f42a0f7b6f288098427a95ddb77
feliposz <feliposz@gmail.com>
2019-04-11 00:40:17 +0000

    Added mailer settings to environment file

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :port           => ENV['SENDGRID_SMTP_PORT'],
+  :address        => ENV['SENDGRID_SMTP_SERVER'],
+  :user_name      => ENV['SENDGRID_SMTP_LOGIN'],
+  :password       => ENV['SENDGRID_SMTP_PASSWORD'],
+  :domain         => ENV['HEROKU_APP_DOMAIN'],
+  :authentication => :plain,
+}
+ActionMailer::Base.delivery_method = :smtp
